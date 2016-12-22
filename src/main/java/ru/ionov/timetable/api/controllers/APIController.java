900edaa291779d7a59a91408a97ba3034c28f8ba
Alexander Ionov <orlicus@gmail.com>
2016-12-23 00:43:02 +0400

    Spring Boot migration

--- src/main/java/ru/ionov/timetable/api/controllers/APIController.java
+++ src/main/java/ru/ionov/timetable/api/controllers/APIController.java
@@ -1,51 +1,30 @@
-package ru.ionov.timetable.api;
+package ru.ionov.timetable.api.controllers;
 
-import com.google.gson.Gson;
-import com.sun.org.apache.regexp.internal.RE;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.ionov.timetable.api.models.Criterion;
 import ru.ionov.timetable.api.models.DateRange;
 import ru.ionov.timetable.api.models.Day;
 import ru.ionov.timetable.api.providers.DataProvider;
 import ru.ionov.timetable.api.utils.DateUtils;
-import spark.Request;
-import spark.Response;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Collections;
 import java.util.List;
 
-import static spark.Spark.*;
+@RestController
+public class APIController {
 
-public class RestService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(APIController.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RestService.class);
-
-    private static final String RESPONSE_TYPE = "application/json";
-
-    private static final String ID_PATH_PARAM = ":id";
-    private static final String CRITERIA_TYPE_PARAM = "criteriaType";
-    private static final String CRITERION_PARAM = "criterion";
-    private static final String FROM_PARAM = "from";
-    private static final String TO_PARAM = "to";
-
-    public static void main(String[] args) {
-
-        Gson gson = new Gson();
-
-        port(8080);
-
-        get("/criteria/" + ID_PATH_PARAM, RestService::getCriteria, gson::toJson);
-        get("/timetable", RestService::getTimetable, gson::toJson);
-    }
-
-    private static List<Criterion> getCriteria(Request request, Response response) {
-        response.type(RESPONSE_TYPE);
-
+    @RequestMapping("/criteria/{id}")
+    public List<Criterion> getCriteria(@PathVariable("id") int criteriaType) {
         try {
-            int criteriaType = Integer.parseInt(request.params(ID_PATH_PARAM));
             return DataProvider.getCriteria(criteriaType);
         } catch (NumberFormatException e) {
             LOGGER.error("Wrong criteria type id format", e);
@@ -56,27 +35,16 @@ public class RestService {
         return Collections.emptyList();
     }
 
-    private static List<Day> getTimetable(Request request, Response response) {
-
-        response.type(RESPONSE_TYPE);
-
-        int criteriaType;
-
-        try {
-            criteriaType = Integer.parseInt(request.queryParams(CRITERIA_TYPE_PARAM));
-        } catch (NumberFormatException e) {
-            LOGGER.error("Wrong criteria type id format", e);
-            return Collections.emptyList();
-        }
-
-        String criterion = request.queryParams(CRITERION_PARAM);
+    @RequestMapping("/timetable")
+    public List<Day> getTimetable(
+            @RequestParam("criteriaType") int criteriaType,
+            @RequestParam("criterion") String criterion,
+            @RequestParam(name = "from", required = false) String from,
+            @RequestParam(name = "to", required = false) String to) {
 
         if (criterion == null || criterion.isEmpty()) {
             return Collections.emptyList();
         }
-
-        String from = request.queryParams(FROM_PARAM);
-        String to = request.queryParams(TO_PARAM);
 
         DateRange dateRange = DateUtils.getSevenDays();
 
