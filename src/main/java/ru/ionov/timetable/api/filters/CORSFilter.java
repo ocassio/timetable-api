01f6e9f327c9c3b39a5909a003654c3fcf95cbd6
Alexander Ionov <orlicus@gmail.com>
2016-12-25 18:10:29 +0400

    Allow cross-origin requests

---
+++ src/main/java/ru/ionov/timetable/api/filters/CORSFilter.java
@@ -0,0 +1,29 @@
+package ru.ionov.timetable.api.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
